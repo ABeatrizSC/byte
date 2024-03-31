1c4ea4b3e625244586863432cbc9a3a13cfc6cfd
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-03-30 22:50:40 -0300

    style: add style in aboutUs-section

--- src/pages/Home/AboutUs/index.jsx
+++ src/pages/Home/AboutUs/index.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import aboutUsImg from '../../../assets/images/teste.jpg'
-import aboutUsImg2 from '../../../assets/images/teste2.jpg'
+import aboutUsImg from '../../../assets/images/aboutUs_img.jpg'
 import './style.css'
 
 export function AboutUs() {
   return (
     <section className='home-section aboutUs-section'>
         <div className='aboutUs-section__image-container'>
-          <img src={aboutUsImg2} alt="" />
+          <img src={aboutUsImg} alt="" />
         </div>
         <div className='aboutUs-section__content'>
             <div className='title-container'>
-                <div className='subtitle-container'>
-                  <div className='subtitle-hr' />
-                  <h3>Nossa história</h3>
-                </div>
-                <h2>Quem somos</h2>
+                <h3>Nossa história</h3>
+                <h2>QUEM SOMOS</h2>
             </div>
-            <p>Fundada há três anos com uma paixão ardente por ingredientes frescos e sabores autênticos, a Byte nasceu da ideia de oferecer uma experiência gastronômica única para os amantes de hambúrgueres artesanais.</p>
+            <div className='text-container'>
+              <p>Fundada há três anos e com uma paixão ardente por ingredientes frescos e sabores autênticos, a <span className='text-highlight'>Byte</span> nasceu da ideia de oferecer uma experiência gastronômica única para os amantes de hambúrgueres artesanais.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam harum sunt ipsum a recusandae dolorem quod deleniti deserunt? Dolor consequuntur iste corporis architecto debitis dignissimos sed ad quaerat voluptates ullam?</p>
+            </div>
         </div>
     </section>
   )
