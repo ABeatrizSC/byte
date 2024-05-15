b66d5c540b3aae5909fcf1e73811abf569d0ba1c
Matheus Júnior <matheusfpjunior@gmail.com>
2024-05-14 23:52:27 -0300

    fix: adjust carousel styles and config, remove arrow buttons

--- src/pages/MenuPage/BannerCarousel/index.tsx
+++ src/pages/MenuPage/BannerCarousel/index.tsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { BannerCarouselProps } from "./type";
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import { AutoplayOptions, PaginationOptions } from "swiper/types";
+import "./style.css";
 
 const BannerCarousel: React.FC<BannerCarouselProps> = (props) => {
-    const { children } = props;
+  const { children } = props;
 
-    return (
-        <div>
-            <Swiper modules={[Navigation, Pagination, Autoplay]} slidesPerView={1} spaceBetween={0} navigation pagination={{ clickable: true }} autoplay>
-                {children.map((child) => (
-                    <SwiperSlide>{child}</SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    )
-}
+  const paginationConfig: PaginationOptions = {
+    clickable: true,
+    bulletClass: "carousel__bullet",
+    bulletActiveClass: "carousel__bullet--active",
+  };
+  const autoplayConfig: AutoplayOptions = {
+    delay: 5000,
+  };
+
+  return (
+    <div>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        spaceBetween={0}
+        pagination={paginationConfig}
+        autoplay={autoplayConfig}
+      >
+        {children.map((child) => (
+          <SwiperSlide>{child}</SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
 export default BannerCarousel;
--- src/pages/MenuPage/index.jsx
+++ src/pages/MenuPage/index.jsx
@@ -1,66 +1,59 @@
-import React from 'react'
-import './style.css'
-import { SwiperSlide, Swiper } from 'swiper/react'
-import 'swiper/css';
-import ProductCard from '../../components/ProductCard';
-import lancheImg from '../../components/Header/Components/CartProduct/lancheExemplo.jpg'
-import BannerCarousel from './BannerCarousel';
-import { Hero } from "../Home/Hero";
-import BannerSlide from './BannerSlide';
-import trojanBanner from './BannerSlide/trojanBanner.jpg';
-import semanaBurguer from './BannerSlide/semana-hamburguer-banner.jpg';
+import "./style.css";
+import "swiper/css";
+import ProductCard from "../../components/ProductCard";
+import lancheImg from "../../components/Header/Components/CartProduct/lancheExemplo.jpg";
+import BannerCarousel from "./BannerCarousel";
+import BannerSlide from "./BannerSlide";
+import trojanBanner from "./BannerSlide/trojanBanner.jpg";
+import semanaBurguer from "./BannerSlide/semana-hamburguer-banner.jpg";
 
 export function MenuPage() {
-
-    const productsMock = [
-        {
-            name: "Trojan",
-            price: "44.99",
-            id: 1,
-            image: lancheImg,
-            size: "MEDIUM"
-        },
-        {
-            name: "João",
-            price: "4400000000.99",
-            id: 12,
-            image: lancheImg,
-            size: "MEDIUM"
-        },
-        {
-            name: "Trojann",
-            price: "44.99",
-            id: 1,
-            image: lancheImg,
-            size: "MEDIUM"
-        },
-        {
-            name: "Trojann",
-            price: "44.99",
-            id: 1,
-            image: lancheImg,
-            size: "MEDIUM"
-        },
-    ]
+  const productsMock = [
+    {
+      name: "Trojan",
+      price: "34.99",
+      id: 1,
+      image: lancheImg,
+      size: "MEDIUM",
+    },
+    {
+      name: "Byte burguer",
+      price: "29.99",
+      id: 12,
+      image: lancheImg,
+      size: "MEDIUM",
+    },
+    {
+      name: "Game Over",
+      price: "24.99",
+      id: 1,
+      image: lancheImg,
+      size: "MEDIUM",
+    },
+    {
+      name: "Ctrl + Eat",
+      price: "49.99",
+      id: 1,
+      image: lancheImg,
+      size: "MEDIUM",
+    },
+  ];
 
   return (
-    <section className='menu-page-section'>
-        <div className='title-container'>
-            <BannerCarousel>
-                <BannerSlide image={trojanBanner} />
-                <BannerSlide image={semanaBurguer} />
-            </BannerCarousel>
+    <section className="menu-page-section">
+      <div className="title-container">
+        <BannerCarousel>
+          <BannerSlide image={trojanBanner} />
+          <BannerSlide image={semanaBurguer} />
+        </BannerCarousel>
 
-            <h3 aria-level={1}>Menu</h3>
-            <h2 aria-level={2}>Lanches</h2>
+        <h3 aria-level={1}>Menu</h3>
+        <h2 aria-level={2}>Lanches</h2>
 
-            <Swiper spaceBetween={8} slidesPerView={2}>
-                {productsMock.map((product, i) => (
-                    <SwiperSlide key={i}><ProductCard {...product} /></SwiperSlide>
-                ))}
-            </Swiper>
-
+        {productsMock.map((product, i) => (
+          <ProductCard key={i} {...product} />
+        ))}
       </div>
     </section>
-  )
+  );
 }
