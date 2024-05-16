a38eab0d516349a6b3edf44423acd3fa493e792d
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-05-16 12:15:54 -0300

    refactor: turn back the Home section in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "./components/Header";
-/* import { Home } from "./pages/Home"; */
+import { Home } from "./pages/Home"; 
 import { Footer } from "./components/Footer";
 import { MenuPage } from "./pages/MenuPage";
 
@@ -9,7 +9,7 @@ export function App() {
   return (
     <>
       <Header />
-      <MenuPage />
+      <Home />
       <Footer />
     </>
   )
