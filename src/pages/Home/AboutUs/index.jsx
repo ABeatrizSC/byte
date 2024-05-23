755a4f67c92ef6567d055641a45ba1c9c61666c4
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-05-23 14:15:05 -0300

    refactor: about us section content

--- src/pages/Home/AboutUs/index.jsx
+++ src/pages/Home/AboutUs/index.jsx
@@ -15,7 +15,7 @@ export function AboutUs() {
             </div>
             <div className='text-container'>
               <p>Fundada há três anos e com uma paixão ardente por ingredientes frescos e sabores autênticos, a <span className='text-highlight'>Byte</span> nasceu da ideia de oferecer uma experiência gastronômica única para os amantes de hambúrgueres artesanais.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam harum sunt ipsum a recusandae dolorem quod deleniti deserunt? Dolor consequuntur iste corporis architecto debitis dignissimos sed ad quaerat voluptates ullam?</p>
+              <p>Nossa lanchonete temática entrega referências ao mundo da tecnologia através de cada item do nosso cardápio. Além disso, o nome <span className='text-highlight'>Byte</span> é um trocadilho que une a unidade básica de dados digitais ao ato de morder ("bite", em inglês).</p>
             </div>
         </div>
     </section>
