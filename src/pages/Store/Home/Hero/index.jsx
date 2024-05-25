070a13bfea5ade147de9936b03b37762b7691847
Matheus Júnior <matheusfpjunior@gmail.com>
2024-05-24 22:41:06 -0300

    poc: create new page structure, segregate store pages from admin pages

---
+++ src/pages/Store/Home/Hero/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+import heroBg from "../../../../assets/images/hero_img.jpg";
+import { useNavigate } from "react-router-dom";
+
+export function Hero() {
+  const navigate = useNavigate();
+  return (
+    <section className="hero-section">
+      <img
+        className="hero-section__img"
+        src={heroBg}
+        alt="Hamburguer 'GameOver' pegando fogo em um fundo de madeira"
+      />
+      <div className="hero-section__text-container">
+        <div className="byte-container">
+          <div className="hr" />
+          <span>BYTE</span>
+        </div>
+        <h1>
+          Sabores que vão além dos
+          <br />
+          <span className="h1--highlight">bites</span>
+        </h1>
+        <div className="subtitle-container">
+          <p>
+            Temos um cardápio recheado de produtos de alta qualidade. Peça já o
+            melhor hambúrguer da cidade!
+          </p>
+          <button onClick={() => navigate("/cardapio")}>Ver cardápio</button>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/Store/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import { MenuPage } from "./MenuPage";
+import { ProductDetails } from "./ProductDetails";
+import { Checkout } from "./Checkout";
+
+export const Store: React.FC = (props) => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cardapio" element={<MenuPage />} />
+        <Route path="/cardapio/:id" element={<ProductDetails />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
